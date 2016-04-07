d1673ed44a3b1ced5582ce333b526278bbc6a52a
jpfriedman18 <jpfriedman18@gmail.com>
2016-04-07 15:01:36 -0400

    Add user name to serializer

--- app/serializers/user_login_serializer.rb
+++ app/serializers/user_login_serializer.rb
@@ -1,6 +1,6 @@
 #
 class UserLoginSerializer < ActiveModel::Serializer
-  attributes :id, :email, :token, :profileable_type, :profileable_id
+  attributes :id, :email, :token, :profileable_type, :profileable_id, :name
 
   def token
     Rails.application.message_verifier(:signed_token).generate(object.token)
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,4 @@
 #
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :email, :profileable_type, :profileable_id
+  attributes :id, :email, :profileable_type, :profileable_id, :name
 end

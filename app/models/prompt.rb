1851e390b14b6abe7e8ee55338d21dec88383693
jpfriedman18 <jpfriedman18@gmail.com>
2016-04-07 15:29:37 -0400

    Add dependent destroy to prompts and responses

--- app/models/prompt.rb
+++ app/models/prompt.rb
@@ -1,4 +1,4 @@
 class Prompt < ActiveRecord::Base
   belongs_to :teacher
-  has_many :prompt_responses
+  has_many :prompt_responses, dependent: :destroy
 end
--- app/models/prompt_response.rb
+++ app/models/prompt_response.rb
@@ -1,5 +1,5 @@
 class PromptResponse < ActiveRecord::Base
   belongs_to :prompt
   belongs_to :student
-  has_one :evaluation
+  has_one :evaluation, dependent: :destroy
 end
